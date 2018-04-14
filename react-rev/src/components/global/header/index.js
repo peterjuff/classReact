01c0de60816f80e26db7efcb8174c4aae75f05cb
Peter Juffernbruch <peterjuffernbruch@gmail.com>
2018-04-14 12:49:30 -0500

    nav and header comp

---
+++ react-rev/src/components/global/header/index.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import Nav from '../nav/'
+
+
+class Header extends Component {
+  render() {
+    return (
+     <header>
+        <Nav/>
+
+     </header>    
+    
+    );
+  }
+}
+
+export default Header;
---
+++ react-rev/src/components/global/nav/index.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+
+class Nav extends Component {
+  render() {
+    return (
+      <nav>     
+          <ul>
+              <li><a href="/">Taco 1</a></li>
+              <li><a href="/">Taco 2</a></li>
+              <li><a href="/">Taco 3</a></li>
+          </ul>
+      </nav>
+    
+    );
+  }
+}
+
+export default Nav;
